9c7b53b5fe1a3d478e911af65b9f6f6412d84c42
XenitoJeje <edwin548vargas@gmail.com>
2024-03-02 16:28:42 -0600

    first commit

---
+++ 2-ventanas-de-dialogo/java.js
@@ -0,0 +1,22 @@
+//comentario
+/*comenatrio 2*/
+ 
+//camelCase / UppelCamelCase / snake_case
+
+// VARIABLES
+// global
+let a; 
+// local
+var A; 
+// invariable, se declara y se inicia
+const c = 0; 
+
+// dataTypes
+let number = 123;
+let string = 'something';
+let bool = true;
+
+// practice 1, holaMundo
+console.log('holaMundo');
+// practice 2, concatenar
+console.Bienvenido('hola ' + number + '! bienvenido a js');
---
+++ 2-ventanas-de-dialogo/javascript.js
@@ -0,0 +1,5 @@
+// practrice 3, prompt & alert
+let nombre = prompt("Bienvenido!\n¿Cuál es tu nombre?");
+let edad = prompt("¿Cuál es tu edad?");
+document.getElementById("userName").innerHTML = nombre;
+document.getElementById("userAge").innerHTML = edad;
